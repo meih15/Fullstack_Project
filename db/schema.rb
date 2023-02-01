6dc2085e89ed5ccd3ecb331c28b6ba372823446a
meih15 <meihuang8328@gmail.com>
2023-02-01 13:12:11 -0500

    reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,59 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_191438) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_140753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cart_items", force: :cascade do |t|
-    t.bigint "cart_id", null: false
-    t.bigint "product_id", null: false
-    t.bigint "quantity", default: 1, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
-    t.index ["product_id"], name: "index_cart_items_on_product_id"
-  end
-
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "category", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "product_name", null: false
-    t.text "description", null: false
-    t.string "product_image", null: false
-    t.bigint "inventory", null: false
-    t.integer "price_cents", default: 0, null: false
-    t.string "price_currency", default: "USD", null: false
-    t.bigint "seller_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "category_id", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
-    t.index ["seller_id"], name: "index_products_on_seller_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.bigint "user_id", null: false
-    t.string "title"
-    t.text "comment"
-    t.integer "rating", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_reviews_on_product_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -76,11 +26,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_191438) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "cart_items", "carts"
-  add_foreign_key "cart_items", "products"
-  add_foreign_key "carts", "users"
-  add_foreign_key "products", "categories"
-  add_foreign_key "products", "users", column: "seller_id"
-  add_foreign_key "reviews", "products"
-  add_foreign_key "reviews", "users"
 end
